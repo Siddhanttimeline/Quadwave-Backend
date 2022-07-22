61a00d8e049486f7e3cadb352546f425279d31d1
Siddhant <siddhanttimeline9@gmail.com>
2022-07-23 00:31:10 +0530

    added

--- src/main/java/com/example/demo/controller/Controller1.java
+++ src/main/java/com/example/demo/controller/Controller1.java
@@ -6,6 +6,8 @@ import java.util.List;
 import com.example.demo.entity.Employee;
 import com.example.demo.service.EmployeeService;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,21 +24,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/controller1")
 public class Controller1 {
 	
+	static Logger logger = LogManager.getLogger(Controller1.class);
+	
+	
+	
 	@Autowired
 	EmployeeService empservice;
 	
 	@GetMapping("/employee")
 	public List<Employee> getAllEmployee(){
+		logger.info(" getAll hit ..........");
 		return empservice.getAllEmployee();
 	}
 	
 	@PostMapping("/employee/add")
 	public void addEmployee(@RequestBody Employee employee) {
+		logger.info(" add hit ..........");
 		empservice.addEmployee(employee);
 	}
 	
 	@DeleteMapping("employee/delete/{id}")
 	public void deleteEmployee(@PathVariable int id) {
+		logger.info(" delete hit ..........");
 		empservice.deleteemployee(id);
 	}
 	
